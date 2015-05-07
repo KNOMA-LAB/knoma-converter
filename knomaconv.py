f87812c07af01fd6e4173a5517cc90eefc42c505
Eduardo Maçan <eduardomacan@gmail.com>
2015-05-07 20:59:33 -0300

    when you hit cancel on a file dialog it does not clean the file path

--- knomaconv.py
+++ knomaconv.py
@@ -5,16 +5,19 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 import time
+import os
 from knoma_converter_ui import Ui_MainWindow
 
 
 def selectInFile():
     dirname = QtWidgets.QFileDialog.getExistingDirectory()
-    ui.lineEdit.setText(dirname)
+    if dirname:
+        ui.lineEdit.setText(dirname)
 
 def selectOutFile():
     dirname = QtWidgets.QFileDialog.getExistingDirectory()
-    ui.lineEdit_2.setText(dirname)
+    if dirname:
+        ui.lineEdit_2.setText(dirname)
 
 def converte():
     ui.progressBar.setRange(0,100)
@@ -31,6 +34,8 @@ ui = Ui_MainWindow()
 ui.setupUi(window)
 
 ui.label_message.setText("Inativo")
+ui.lineEdit.setText(os.path.join(__file__,"input"))
+ui.lineEdit_2.setText(os.path.join(__file__,"output"))
 
 ui.pushButton.clicked.connect(selectInFile)
 ui.pushButton_2.clicked.connect(selectOutFile)
