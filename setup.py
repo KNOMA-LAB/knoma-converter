3961b1d0bfe28702038bb5c0d72b09a170e2476c
Eduardo Maçan <eduardomacan@gmail.com>
2015-05-11 17:28:14 -0300

    Added setup.py , created by cx_Freeze

---
+++ setup.py
@@ -0,0 +1,19 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages=[], excludes=[])
+
+import sys
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+
+executables = [
+    Executable('knomaconv.py', base=base)
+]
+
+setup(name='knoma-converter',
+      version='1.0',
+      description='knoma imagemagick frontend',
+      options=dict(build_exe=buildOptions),
+      executables=executables)
