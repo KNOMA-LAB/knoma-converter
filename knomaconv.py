b15e3db72e5fde36702df0182194623fa8ec014c
Eduardo Maçan <eduardomacan@gmail.com>
2015-06-29 21:44:00 -0300

    Excluído menu de "Ajuda"

--- knomaconv.py
+++ knomaconv.py
@@ -7,8 +7,20 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from datetime import datetime
 import os
 from knoma_converter_ui import Ui_MainWindow
+from knoma_about_ui import Ui_Dialog
 import subprocess
 import re
+
+def selectInThumbFile():
+    fileDialog = QtWidgets.QFileDialog()
+
+    dirname = fileDialog.getOpenFileNames(caption='Selectione a imagem para Thumbnail',
+                                          filter='Imagens (*.jpg *.png *.gif *.tif)'
+                                          )
+
+    if dirname:
+        ui.lineEdit_3.setText(';'.join(dirname[0]))
+
 
 def selectInFile():
     fileDialog = QtWidgets.QFileDialog()
@@ -81,6 +93,30 @@ def converte():
     ui.plainTextEdit.appendPlainText("Conversão concluída em {}".format(datetime.now()))
     #ui.label_message.setText("Concluído")
 
+def makethumb():
+
+    #TODO: CHECK EXISTENCE
+    path = ui.lineEdit_3.text()
+    ui.plainTextEdit_2.clear()
+    ui.plainTextEdit_2.appendPlainText("Criando Thumbnail em {}".format(datetime.now()))
+
+    # Let's pick only the images from the directory
+
+    namecomponents=path.split('.')
+    namecomponents[-1] = 'jpg'
+    namecomponents[-2] = namecomponents[-2] + '-thumb'
+    outf = '.'.join(namecomponents)
+
+    ui.plainTextEdit.appendPlainText("Fazendo thumbnail de {} em {}".format(path,outf))
+
+    command = 'convert "{}" -thumbnail 160x160^ -gravity center -extent 160x160 "{}"'.format( path, outf )
+    #TODO: change call to not use shell
+    output = subprocess.check_output(command, shell=True)
+    #subprocess.call(["convert",os.path.join(inpath,f),'-define','tiff:tile-geometry=256x256',
+    #                 '-compress', 'jpeg',"'ptif:%s'"% os.path.join(outpath,outf) ])
+    ui.plainTextEdit.appendPlainText("Conversão concluída em {}".format(datetime.now()))
+
+
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
 
@@ -92,10 +128,15 @@ ui.setupUi(window)
 
 ui.pushButton.clicked.connect(selectInFile)
 ui.pushButton_2.clicked.connect(selectOutFile)
+ui.pushButton_3.clicked.connect(selectInThumbFile)
+
 ui.pushButton_go.clicked.connect(converte)
+ui.pushButton_4.clicked.connect(makethumb)
 
 ui.progressBar.setRange(0,100)
 ui.progressBar.setValue(0)
+
+ui.actionClose.triggered.connect(QtCore.QCoreApplication.instance().quit)
 
 window.show()
 sys.exit(app.exec_())
